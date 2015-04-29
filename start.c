ecc375d6fb86740091d4e221293c2930b6f62371
Jonw27 <seuvend@gmail.com>
2015-04-29 22:44:26 +0000

    c

---
+++ start.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  puts("Hello, world!");
+  int sum(int x, int y) {
+    int result = x + y;
+    return result;
+  }
+  int random = x + y; 
+  int count = 10;
+  return 0;
+}
